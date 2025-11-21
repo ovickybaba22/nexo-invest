24977e6a5d6af6c89e0f1873f6c917f7cdaca6c8
vikkysam123 <149323658+vikkysam123@users.noreply.github.com>
2025-11-21 02:55:05 -0800

    Implement new Nexo Daily/Weekly/APY plans and ROI engine

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,19 +2,20 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ * @extends Factory<User>
  */
 class UserFactory extends Factory
 {
     /**
      * The current password being used by the factory.
      */
-    protected static ?string $password;
+    protected static ?string $password = null;
 
     /**
      * Define the model's default state.
@@ -24,8 +25,8 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => 'Seed User',
+            'email' => 'seed@example.com',
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
